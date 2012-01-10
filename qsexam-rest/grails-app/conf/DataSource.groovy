da79d1aaf312b4bba0786d6c882fc265a0e6bf8c
Jacob Robertson <g.jacob.robertson@gmail.com>
2012-01-10 17:01:02 -0500

    Connected to data source

--- qsexam-rest/grails-app/conf/DataSource.groovy
+++ qsexam-rest/grails-app/conf/DataSource.groovy
@@ -1,8 +1,11 @@
 dataSource {
     pooled = true
-    driverClassName = "org.h2.Driver"
-    username = "sa"
-    password = ""
+	dbCreate = "validate"
+	driverClassName = "com.mysql.jdbc.Driver"
+	dialect = org.hibernate.dialect.MySQL5InnoDBDialect
+	url = "jdbc:mysql://qsexamdb.ckxhfpla2vr0.us-east-1.rds.amazonaws.com:3306/metricsdb"
+    username = "qsexamuser"
+    password = "e8KjivnWvd8H"
 }
 hibernate {
     cache.use_second_level_cache = true
@@ -13,21 +16,14 @@ hibernate {
 environments {
     development {
         dataSource {
-            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
-            url = "jdbc:h2:mem:devDb;MVCC=TRUE"
         }
     }
     test {
         dataSource {
-            dbCreate = "update"
-            url = "jdbc:h2:mem:testDb;MVCC=TRUE"
         }
     }
     production {
         dataSource {
-            dbCreate = "update"
-            url = "jdbc:h2:prodDb;MVCC=TRUE"
-            pooled = true
             properties {
                maxActive = -1
                minEvictableIdleTimeMillis=1800000
