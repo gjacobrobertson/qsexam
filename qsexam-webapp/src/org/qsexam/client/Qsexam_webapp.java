6e4eeb0c599183de101b6f64a1fbd838b33edad3
Jacob Robertson <g.jacob.robertson@gmail.com>
2012-01-11 21:50:23 -0500

    JSON hitting SOP bug

--- qsexam-webapp/src/org/qsexam/client/Qsexam_webapp.java
+++ qsexam-webapp/src/org/qsexam/client/Qsexam_webapp.java
@@ -1,12 +1,17 @@
 package org.qsexam.client;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.core.client.JsArray;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.KeyCodes;
 import com.google.gwt.event.dom.client.KeyPressEvent;
 import com.google.gwt.event.dom.client.KeyPressHandler;
+import com.google.gwt.http.client.Request;
+import com.google.gwt.http.client.RequestBuilder;
+import com.google.gwt.http.client.RequestCallback;
+import com.google.gwt.http.client.RequestException;
+import com.google.gwt.http.client.Response;
+import com.google.gwt.http.client.URL;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.HorizontalPanel;
@@ -17,7 +22,7 @@ import com.google.gwt.user.client.ui.VerticalPanel;
 
 public class Qsexam_webapp implements EntryPoint {
 	
-	private static final String JSON_URL = ""
+	private static final String JSON_URL = "http://localhost:8080/qsexam-rest/store/json?name=";
 	private Store store;
 	private HorizontalPanel findPanel = new HorizontalPanel();
 	private TextBox storeNameTextBox = new TextBox();
@@ -26,6 +31,7 @@ public class Qsexam_webapp implements EntryPoint {
 	private Label nameLabel = new Label();
 	private Label contactLabel = new Label();
 	private FlexTable salesFlexTable = new FlexTable();
+	private Label errorMsgLabel = new Label();
 	
 	public void onModuleLoad() {
 		//Assemble Find Store panel
@@ -41,6 +47,8 @@ public class Qsexam_webapp implements EntryPoint {
 		salesFlexTable.setText(0, 5, "On Hand");
 		
 		//Assemble Main panel
+		errorMsgLabel.setVisible(false);
+		mainPanel.add(errorMsgLabel);
 		mainPanel.add(findPanel);
 		mainPanel.add(nameLabel);
 		mainPanel.add(contactLabel);
@@ -70,11 +78,48 @@ public class Qsexam_webapp implements EntryPoint {
 	}
 
 	private void findStore() {
-		// TODO Auto-generated method stub
+		String url = JSON_URL + storeNameTextBox.getText().trim();
+		System.out.println(url);
 		
+		RequestBuilder builder = new RequestBuilder(RequestBuilder.GET, URL.encode(url));
+		
+		try {
+			Request request = builder.sendRequest(null, new RequestCallback() {
+				public void onError(Request request, Throwable exception) {
+					displayError("Couldn't retrieve JSON");
+				}
+				
+				public void onResponseReceived(Request request, Response response) {
+					if (200 == response.getStatusCode()) {
+						updateStore(asStore(response.getText()));
+					} else {
+						displayError("Couldn't retrieve JSON (" + response.getStatusCode() + ")");
+					}
+				}
+			});
+		} catch (RequestException e) {
+			displayError("Couldn't retrieve JSON");
+		}
 	}
 	
-	private final native JsArray<Store> asArrayOfStore(String json) /* - {
+	private void updateStore(Store store) {
+		System.out.println("Showing: " + store.getName());
+		nameLabel.setText(store.getName());
+		//Clear errors
+		errorMsgLabel.setVisible(false);
+	}
+
+	/**
+	 * If can't get JSON, display error message.
+	 * 
+	 * @param string
+	 */
+	private void displayError(String error) {
+		errorMsgLabel.setText("Error: " + error);
+		errorMsgLabel.setVisible(true);
+	}
+
+	private final native Store asStore(String json) /*-{
 		return eval(json);
 	}-*/;
 }
